cf05f890e8010527573ae573a53b8e30e3a8a2da
Neme <66285758+nemedrack@users.noreply.github.com>
2022-07-21 16:13:50 -0400

    Try Jueves
    owo

--- src/main/java/Main/BotListener.java
+++ src/main/java/Main/BotListener.java
@@ -19,6 +19,19 @@ public class BotListener extends ListenerAdapter {
             event.getTextChannel().sendMessage("https://i.imgur.com/kf7WnWY.png").queue();
         }
     }
-
+    
+    private void takoJueves(MessageReceivedEvent event){
+        String messageSend = event.getMessage().getContentRaw();
+        if(messageSend.contains("jueves") || messageSend.contains("thursday")){
+            Calendar cal = Calendar.getInstance();
+            Date date = new Date();
+            cal.setTime(date);
+            if(cal.get(Calendar.DAY_OF_WEEK) == Calendar.THURSDAY){
+                event.getTextChannel().sendMessage("https://c.tenor.com/riA830QHvC8AAAAC/feliz-jueves-asuka.gif").queue();
+            }else{
+                event.getTextChannel().sendMessage("Si no es jueves no es un dia feliz").queue();
+            }
+        }
+    }
 
 }
